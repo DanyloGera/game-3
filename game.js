a2f348d10d4140b3cfa30a554bc2f0da611322e5
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-04-14 19:27:33 +0300

    Update game.js

--- game.js
+++ game.js
@@ -84,17 +84,12 @@ function itemHandler(player, item) {
 // when the player collects the badge at the end of the game
 function badgeHandler(player, badge) {
   badge.kill();
-  sprite.events.add(destroySprite, 'grandma');
   // var btn = document.getElementById('btn-next');
   // btn.removeAttribute("disabled");
   // btn.style.border = "2px solid #000";
   won = true;
 }
-function destroySprite (sprite) {
 
-    sprite.destroy();
-
-}
 // setup game when the web page loads
 window.onload = function () {
   game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
